2f73db3ca79fdbb05f23e41c0e9ad8480547b335
mortezai <morteza2006g@gmail.com>
2025-08-15 22:39:12 +0330

    feat(init): add init files

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0"
+
+from.utils import Embedder, clean_text
+
+__all__ = ["Embedder", "clean_text"]
---
+++ src/utils/__init__.py
@@ -0,0 +1,4 @@
+from .embedding_utils import Embedder
+from .text_cleaning import clean_text
+
+__all__ = ["Embedder", "clean_text"]
