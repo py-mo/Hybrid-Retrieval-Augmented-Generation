8a26e884d7b56a609f762e4acfcdb57a7d39b565
mortezai <morteza2006g@gmail.com>
2025-08-31 14:55:08 +0330

    feat(init): update init files

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0"
 
-from.utils import Embedder, clean_text
+from.utils import Embedder, clean_text, PDFTextExtractor
 
-__all__ = ["Embedder", "clean_text"]
+__all__ = ["Embedder", "clean_text", "PDFTextExtractor"]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
 from .embedding_utils import Embedder
 from .text_cleaning import clean_text
+from .text_extracting import PDFTextExtractor
 
-__all__ = ["Embedder", "clean_text"]
+__all__ = ["Embedder", "clean_text", "PDFTextExtractor"]
